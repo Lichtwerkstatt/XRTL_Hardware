bdf7bf5c86e685bc89ae4841b20551b617173abe
doomke <94845686+doomke@users.noreply.github.com>
2023-04-11 21:57:30 +0200

    added new command

--- firmware/lib/XRTL/src/common/XRTLcommand.h
+++ firmware/lib/XRTL/src/common/XRTLcommand.h
@@ -27,10 +27,121 @@ public:
     void setViaSerial();
 };
 
+class XRTLcommand2 : public XRTLcommand
+{
+private:
+    String *key = NULL;
+    XRTLval *val = NULL;
+
+    void clear()
+    {
+        if (key != NULL)
+            delete key;
+        if (val != NULL)
+            delete val;
+    }
+
+public:
+    ~XRTLcommand2()
+    {
+        clear();
+    }
+
+    void set(const String &controlId, const String &controlKey, bool controlVal)
+    {
+        clear();
+        id = controlId;
+        key = new String(controlKey);
+        val = new XRTLvalBool(controlVal);
+    }
+
+    void set(const String &controlId, const String &controlKey, long controlVal)
+    {
+        clear();
+        id = controlId;
+        key = new String(controlKey);
+        val = new XRTLvalInt(controlVal);
+    }
+
+    void set(const String &controlId, const String &controlKey, double controlVal)
+    {
+        clear();
+        id = controlId;
+        key = new String(controlKey);
+        val = new XRTLvalFloat(controlVal);
+    }
+
+    void set(const String &controlId, const String &controlKey, String controlVal)
+    {
+        clear();
+        id = controlId;
+        key = new String(controlKey);
+        val = new XRTLvalString(controlVal);
+    }
+
+    void saveSettings(JsonObject& settings)
+    {
+        val->passValue(*key, settings);
+    }
+
+    void fillCommand(JsonObject& command)
+    {
+        command["controlId"] = id;
+        saveSettings(command);
+    }
+    
+    void setViaSerial()
+    {
+        String newId = serialInput("controlId: ");
+        String newKey = serialInput("control key: ");
+
+        Serial.println("available types");
+        Serial.println("0: bool");
+        Serial.println("1: int");
+        Serial.println("2: float");
+        Serial.println("3: string");
+        Serial.println("");
+        switch (serialInput("control value type: ").toInt())
+        {
+        case (0):
+        {
+            String input = serialInput("value: ");
+            if (input == "true" || input == "TRUE" || input == "y" || input == "1")
+            {
+                set(newId, newKey, true);
+            }
+            else if (input == "false" || input == "FALSE" || input == "n" || input == "0")
+            {
+                set(newId, newKey, false);
+            }
+            break;
+        }
+        case (1):
+        {
+            long input = serialInput("value: ").toInt();
+            set(newId, newKey, input);
+            break;
+        }
+        case (2):
+        {
+            double input = serialInput("value: ").toDouble();
+            set(newId, newKey, input);
+            break;
+        }
+        case (3):
+        {
+            String input = serialInput("value: ");
+            set(newId, newKey, input);
+            break;
+        }
+        }
+    }
+};
+
 class XRTLdisposableCommand : public XRTLcommand
 {
 private:
-    const String* keys[8];
+    String* keys[8];
     XRTLval* vals[8];
     uint8_t keyCount = 0;
 
